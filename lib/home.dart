17998f7cc192ab2c0fa265fae2f6fa303dc5099a
AlanNegalho <alandevads@gmail.com>
2023-05-09 20:51:45 -0300

    style

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'api.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -31,26 +30,22 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                'Poke Bola',
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-              /* Theme.of(context).textTheme.titleLarge) */
-              SizedBox(
-                width: 10,
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'Go!',
-                    style: TextStyle(color: Color(0xFF2E2B2B), fontSize: 15),
+                  const Text(
+                    'Poke Bola',
+                    style: TextStyle(color: Colors.black, fontSize: 35),
                   ),
                   Image.asset(
                     "assets/poke.png",
                     width: double.tryParse("50"),
                   ),
                 ],
+              ),
+              /* Theme.of(context).textTheme.titleLarge) */
+              const SizedBox(
+                width: 10,
               ),
             ],
           ),
@@ -124,7 +119,7 @@ class _PokemonsListState extends State<PokemonsList> {
                     Row(
                       children: [
                         Text(widget.pokemons[index].type.toString()),
-                        SizedBox(
+                        const SizedBox(
                           width: 10,
                         ),
                         Text(
@@ -153,7 +148,8 @@ class _PokemonsListState extends State<PokemonsList> {
                     context,
                     MaterialPageRoute<Widget>(builder: (BuildContext context) {
                       return Scaffold(
-                        backgroundColor: Color.fromARGB(255, 119, 221, 122),
+                        backgroundColor:
+                            const Color.fromARGB(255, 119, 221, 122),
                         appBar:
                             AppBar(title: const Text('Detalhes do Pokemon')),
                         body: Hero(
@@ -172,11 +168,11 @@ class _PokemonsListState extends State<PokemonsList> {
                                     ],
                                   ),
                                   Container(
-                                    padding: EdgeInsets.all(20),
+                                    padding: const EdgeInsets.all(20),
                                     width: 407,
                                     height: 400,
-                                    decoration: BoxDecoration(
-                                      boxShadow: const [
+                                    decoration: const BoxDecoration(
+                                      boxShadow: [
                                         BoxShadow(
                                             color: Colors.black,
                                             offset: Offset(1, 2.0),
@@ -195,34 +191,34 @@ class _PokemonsListState extends State<PokemonsList> {
                                       children: [
                                         Text(
                                             "Nome: ${widget.pokemons[index].name}",
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                                 color: Color.fromARGB(
                                                     255, 253, 1, 1),
                                                 fontSize: 20)),
-                                        Divider(),
+                                        const Divider(),
                                         Text(
                                             "N°: ${widget.pokemons[index].num}",
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                                 color: Colors.black,
                                                 fontSize: 15)),
                                         Text(
                                             "Tipo: ${widget.pokemons[index].type}",
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                                 color: Colors.black,
                                                 fontSize: 15)),
                                         Text(
                                             "Peso: ${widget.pokemons[index].weight}",
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                                 color: Colors.black,
                                                 fontSize: 15)),
                                         Text(
                                             "Altura: ${widget.pokemons[index].height}",
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                                 color: Colors.black,
                                                 fontSize: 15)),
                                         Text(
                                             "Fraco Contra: ${widget.pokemons[index].weaknesses}",
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                                 color: Colors.black,
                                                 fontSize: 15)),
                                       ],
@@ -245,180 +241,3 @@ class _PokemonsListState extends State<PokemonsList> {
     );
   }
 }
-
-
-
-//import 'package:flutter/material.dart';
-
-// import 'api.dart';
-
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-//   final String title;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       body: FutureBuilder<List<Dados>>(
-//         future: dados(),
-//         builder: (context, snapshot) {
-//           if (snapshot.hasError) {
-//             return const Center(
-//               child: Text('An error has occurred!'),
-//             );
-//           } else if (snapshot.hasData) {
-//             return PokemonsList(pokemons: snapshot.data!);
-//           } else {
-//             return const Center(
-//               child: CircularProgressIndicator(),
-//             );
-//           }
-//         },
-//       ),
-//     );
-//   }
-// }
-
-// class PokemonsList extends StatelessWidget {
-//   const PokemonsList({Key? key, required this.pokemons}) : super(key: key);
-
-//   final List<Dados> pokemons;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GridView.builder(
-//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//         crossAxisCount: 2,
-//       ),
-//       itemCount: pokemons.length,
-//       itemBuilder: (context, index) {
-//         return Container(
-//           child: Container(
-//             child: ListTile(
-//               title: Image.network("${pokemons[index].img}", height: 200),
-//               subtitle: Text(
-//                 "${pokemons[index].name}",
-//                 style: const TextStyle(
-//                   color: Colors.black,
-//                   fontSize: 17.0,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               onTap: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute<Widget>(builder: (BuildContext context) {
-//                     return Scaffold(
-//                       appBar: AppBar(title: const Text('Detalhes do Pokemon')),
-//                       body: Hero(
-//                         tag: 'Detalhes Pokemon',
-//                         child: Row(
-//                           children: [
-//                             Column(
-//                               children: [
-//                                 Row(
-//                                   children: [
-//                                     Image.network(
-//                                       "${pokemons[index].img}",
-//                                       height: 200,
-//                                     ),
-//                                   ],
-//                                 ),
-//                                 Expanded(
-//                                   child: Padding(
-//                                     padding: const EdgeInsets.only(left: 8.0),
-//                                     child: Column(
-//                                       crossAxisAlignment:
-//                                           CrossAxisAlignment.start,
-//                                       children: [
-//                                         Text(
-//                                             "Nome do Pokemon: ${pokemons[index].name}"),
-//                                         Text("N°: ${pokemons[index].num}"),
-//                                         Text("Tipo: ${pokemons[index].type}"),
-//                                         Text("Peso: ${pokemons[index].weight}"),
-//                                         Text(
-//                                             "Altura: ${pokemons[index].height}"),
-//                                         Text(
-//                                             "Fraco Contra: ${pokemons[index].weaknesses}"),
-//                                       ],
-//                                     ),
-//                                   ),
-//                                 )
-//                               ],
-//                             ),
-//                           ],
-//                         ),
-//                       ),
-//                     );
-//                   }),
-//                 );
-//               },
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
-
-
-// // import 'package:flutter/material.dart';
-
-// // import 'api.dart';
-
-// // class MyHomePage extends StatelessWidget {
-// //   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-// //   final String title;
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         title: Text(title),
-// //       ),
-// //       body: FutureBuilder<List<Dados>>(
-// //         future: dados(),
-// //         builder: (context, snapshot) {
-// //           if (snapshot.hasError) {
-// //             return const Center(
-// //               child: Text('An error has occurred!'),
-// //             );
-// //           } else if (snapshot.hasData) {
-// //             return PokemonsList(pokemons: snapshot.data!);
-// //           } else {
-// //             return const Center(
-// //               child: CircularProgressIndicator(),
-// //             );
-// //           }
-// //         },
-// //       ),
-// //     );
-// //   }
-// // }
-
-// // class PokemonsList extends StatelessWidget {
-// //   const PokemonsList({Key? key, required this.pokemons}) : super(key: key);
-
-// //   final List<Dados> pokemons;
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return GridView.builder(
-// //         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-// //           crossAxisCount: 2,
-// //         ),
-// //         itemCount: pokemons.length,
-// //         itemBuilder: (context, index) {
-// //           return Image.network(
-// //             "${pokemons[index].img}",
-// //           );
-// //         });
-// //   }
-// // }
-
